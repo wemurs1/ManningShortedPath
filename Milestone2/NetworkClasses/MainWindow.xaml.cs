a4e316da04666b945c8a7c97a13300ad43b96199
Stuart Murray <stuart.murray.dev@outlook.com>
2022-08-23 13:58:44 +0100

    reset xaml for milestone 2

--- Milestone2/NetworkClasses/MainWindow.xaml.cs
+++ Milestone2/NetworkClasses/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Point = System.Drawing.Point;
+using Microsoft.Win32;
 
 namespace NetworkClasses
 {
@@ -27,168 +28,54 @@ namespace NetworkClasses
             InitializeComponent();
         }
 
+        private Network MyNetwork = new Network();
 
-
-        // Build small test networks.
-
-        private void net1Button_Click(object sender, RoutedEventArgs e)
-
+        private void OpenCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
+            e.CanExecute = true;
+        }
 
-            Network network = new Network();
+        private void OpenCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
 
+                dialog.DefaultExt = ".net";
+                dialog.Filter = "Network Files|*.net|All Files|*.*";
 
+                // Display the dialog.
+                bool? result = dialog.ShowDialog();
 
-            Node a = new Node(network, new Point(20, 20), "A");
+                if (result == true)
+                {
+                    // Open the network.
 
-            Node b = new Node(network, new Point(120, 120), "B");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
 
+                MyNetwork = new Network();
+            }
 
+            // Display the network.
+            DrawNetwork();
+        }
 
-            Link link_a_b = new Link(network, a, b, 10);
+        private void DrawNetwork()
+        {
+            // Remove any previous drawing.
+            
 
-
-
-            // Validate the network.
-
-            ValidateNetwork(network);
+            // Make the network draw itself.
 
         }
 
-
-
-        private void net2Button_Click(object sender, RoutedEventArgs e)
-
+        private void ExitCommand_Executed(object sender, RoutedEventArgs e)
         {
-
-            Network network = new Network();
-
-
-
-            Node a = new Node(network, new Point(20, 20), "A");
-
-            Node b = new Node(network, new Point(120, 20), "B");
-
-            Node c = new Node(network, new Point(20, 120), "C");
-
-            Node d = new Node(network, new Point(120, 120), "D");
-
-
-
-            Link link_a_b = new Link(network, a, b, 10);
-
-            Link link_b_d = new Link(network, b, d, 15);
-
-            Link link_a_c = new Link(network, a, c, 20);
-
-            Link link_c_d = new Link(network, c, d, 25);
-
-
-
-            // Validate the network.
-
-            ValidateNetwork(network);
-
-        }
-
-
-
-        private void net3Button_Click(object sender, RoutedEventArgs e)
-
-        {
-
-            Network network = new Network();
-
-
-
-            Node a = new Node(network, new Point(20, 20), "A");
-
-            Node b = new Node(network, new Point(120, 20), "B");
-
-            Node c = new Node(network, new Point(20, 120), "C");
-
-            Node d = new Node(network, new Point(120, 120), "D");
-
-
-
-            Link link_a_b = new Link(network, a, b, 10);
-
-            Link link_b_d = new Link(network, b, d, 15);
-
-            Link link_a_c = new Link(network, a, c, 20);
-
-            Link link_c_d = new Link(network, c, d, 25);
-
-
-
-            Link link_b_a = new Link(network, b, a, 11);
-
-            Link link_d_b = new Link(network, d, b, 16);
-
-            Link link_c_a = new Link(network, c, a, 21);
-
-            Link link_d_c = new Link(network, d, c, 26);
-
-
-
-            // Validate the network.
-
-            ValidateNetwork(network);
-
-        }
-
-
-
-        // Serialize the network, save it into a file,
-
-        // read it back from the file, reserialize it,
-
-        // and compare the two serializations.
-
-        private void ValidateNetwork(Network network)
-
-        {
-
-            // Serialize.
-
-            string serialization1 = network.Serialization();
-
-
-
-            // Save into a file,
-
-            network.SaveIntoFile("test_network.net");
-
-
-
-            // Read it back from the file.
-
-            network.ReadFromFile("test_network.net");
-
-
-
-            // Reserialize.
-
-            string serialization2 = network.Serialization();
-
-
-
-            // Display the serialization.
-
-            netTextBox.Text = network.Serialization();
-
-
-
-            // Compare the two serializations.
-
-            if (serialization1 == serialization2)
-
-                statusLabel.Content = "OK";
-
-            else
-
-                statusLabel.Content = "Serializations do not match";
-
+            Close();
         }
     }
 }
